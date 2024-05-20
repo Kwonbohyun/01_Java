e6937f0ab4bec4391ffcd68acb93082ca9d61ec2
Kwonbohyun <kwon18923@gmail.com>
2024-05-20 18:03:47 +0900

    수업내용: 난수 API

---
+++ chap04-control-flow-lecture-source/src/main/java/com/ohgiraffers/section01/conditional/pratice/pratice1.java
@@ -0,0 +1,30 @@
+package com.ohgiraffers.section01.conditional.pratice;
+
+import java.util.Scanner;
+
+public class pratice1 {
+
+
+    public void mawer(){
+        int age = 9;
+        int height = 110;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter age");
+        age = sc.nextInt();
+        System.out.println("Enter height");
+        height = sc.nextInt();
+        if(age <= 9);
+        if(height <= 100){}
+
+
+        }
+
+
+
+
+        }
+
+
+
+
---
+++ chap04-control-flow-lecture-source/src/main/java/com/ohgiraffers/section02/looping/Application.java
@@ -0,0 +1,33 @@
+package com.ohgiraffers.section02.looping;
+
+// 실행용 클래스
+public class Application {
+
+    public static void main(String[] args) {
+
+        A_for a_for = new A_for();
+
+        //a_for.testSimpleForStatement();
+        //a_for.testForExample1();
+        //a_for.testExample2();
+        //a_for.testExample3();
+        //a_for.testExample4();
+        //a_for.printSimpleGugudan();
+
+        //A_mestedfor a_nestedFor = new A_mestedfor();
+        //a_nestedFor.printGugudanFromTwoToNine();
+        //a_nestedFor.printUpgradeGugudanFromTwoToNine();
+        //a_nestedFor.printStarInputRowTimes();
+        //a_nestedFor.pirntTriangleStars();
+
+        B_while b_while = new B_while();
+        //b_while.testSimpleWhileStatement();
+        //b_while.testWhileExample1();
+        //b_while.testWhile'example2();
+        //b_while.testWhileExample3();
+
+        //C_doowhile c_doWhile = new C_doowhile();
+        //c_doWhile.testDowhileStatement();
+
+    }
+}
