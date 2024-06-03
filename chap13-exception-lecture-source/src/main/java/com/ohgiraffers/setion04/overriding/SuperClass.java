934374996bd51b1e06b244c7551727168c63170a
Kwonbohyun <kwon18923@gmail.com>
2024-06-03 18:11:15 +0900

    chap 15

---
+++ chap13-exception-lecture-source/src/main/java/com/ohgiraffers/setion04/overriding/SuperClass.java
@@ -0,0 +1,11 @@
+package com.ohgiraffers.setion04.overriding;
+
+import java.io.IOException;
+
+public class SuperClass {
+
+    public void method()throws IOException {}
+
+
+
+}
---
+++ chap15-enum-lecture-source/src/main/java/com/ohgiraffers/section03/grammar/UserRole1.java
@@ -0,0 +1,27 @@
+package com.ohgiraffers.section03.grammar;
+
+import java.util.Locale;
+
+public enum UserRole1 {
+    // Enum은 상수 하나하나가 인스턴스화 될 수 있다.
+    GUEST,
+    CONSUMER,
+    PRODUCER,
+    ADMIN;
+
+    /*
+     * 기본 생성자를 가질 수 있다.
+     * default와 private 접근제한 사용 가능하지만, 외부에서 호출은 불가능(묵시적으로 private
+     * enum 타입은 고정된 상수들으 ㅣ집합으로, 런타임이 아닌, 컴파일시에 모든 값이 결정되어
+     *
+     * */
+
+    UserRole1() {
+    }
+
+    public String getNameToLowerCase() {
+    return this.name().toLowerCase();
+
+    }
+
+}
